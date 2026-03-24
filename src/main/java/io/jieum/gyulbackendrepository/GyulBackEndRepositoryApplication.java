da257bf1386c8a3c01faa868d140ec7f3692db77
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-03-24 20:29:43 +0900

    Initial commit

---
+++ src/main/java/io/jieum/gyulbackendrepository/GyulBackEndRepositoryApplication.java
@@ -0,0 +1,13 @@
+package io.jieum.gyulbackendrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyulBackEndRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyulBackEndRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/jieum/gyulbackendrepository/GyulBackEndRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jieum.gyulbackendrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyulBackEndRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
