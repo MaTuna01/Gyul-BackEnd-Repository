d1868dea6df4e46868a222b909c15d749cdf4878
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-04-28 13:21:57 +0900

    [#1] SecurityConfig 설정 및 api 경로 오탈자 수정

--- src/main/java/io/jieum/gyulbackendrepository/domain/auth/form/controller/SignUpController.java
+++ src/main/java/io/jieum/gyulbackendrepository/domain/auth/form/controller/SignUpController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 public class SignUpController {
 
     SignUpService signUpService;
--- src/main/java/io/jieum/gyulbackendrepository/global/config/SecurityConfig.java
+++ src/main/java/io/jieum/gyulbackendrepository/global/config/SecurityConfig.java
@@ -1,15 +1,71 @@
 package io.jieum.gyulbackendrepository.global.config;
 
+import io.jieum.gyulbackendrepository.global.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http    // CSRF 비활성화 (토큰 기반의 무상태성 서버)
+                .csrf(csrf -> csrf.disable())
+                // CORS 설정
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // 세션정책 stateless(토큰기반 무상태성)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
+                        "/api/v1/signup",
+                        "/auth/signin",
+                        "/auth/reissue",
+                        "/login/oauth2/**",
+                        "/oauth2/**"
+                ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 프론트엔드 주소
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
